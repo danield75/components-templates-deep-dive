2aadaf39acbee8dd2bb22a4c425677da97ade9a8
Daniel Drion <daniel.drion@telenet.be>
2025-02-07 13:47:02 +0100

    Decorator-based queries and lifecycle hooks

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterContentInit,
   Component,
   contentChild,
   ContentChild,
@@ -22,7 +23,7 @@ import {
     '(click)': 'onClick()',
   },
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClick() {
   //   console.log('Control clicked');
@@ -34,6 +35,10 @@ export class ControlComponent {
   // >;
   private control =
     contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit(): void {
+    // ...
+  }
 
   onClick() {
     console.log('Control clicked');
