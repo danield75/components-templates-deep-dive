d5a68ab78984451f673063a556f83372eecf9bc5
Daniel Drion <daniel.drion@telenet.be>
2025-02-24 12:14:40 +0100

    Implement Mark as completed

--- src/app/dashboard/support-tickets/support-tickets.component.ts
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -23,4 +23,14 @@ export class SupportTicketsComponent {
 
     this.tickets.push(ticket);
   }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' };
+      }
+
+      return ticket;
+    });
+  }
 }
